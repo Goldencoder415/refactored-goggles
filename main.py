793753529fd343c054d206e27af8ed7d6393efeb
Goldencoder415 <hamzakhakwani2016@gmail.com>
2025-11-21 17:45:45 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+class hamza():
+    def __init__(self, name,gender):
+        self.name = name
+        self.gender = gender
+
+    def display(self):
+        print(f"Name: {self.name}, Gender: {self.gender}")
